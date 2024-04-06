994fbe2c1c7721713a5a17bcef58e15f9b105883
Nakidai <plaza521@inbox.ru>
2024-04-06 13:59:21 +0300

    Some improvments
    - Add blank lines between function because of PEP8
    - Simplify game
    - Change algorythm

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import disnake
 from disnake.ext import commands
 import random
 
+
 intents = disnake.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
@@ -11,45 +12,36 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print(f'Бот {bot.user} подключен к Discord!')
 
-@bot.slash_command(name="игра", description="Начать игру Камень, ножницы, бумага")
-async def start_game(ctx):
-    bot.game_started = True
-    await ctx.send("Игра началась! Выберите: камень, ножницы или бумагу.")
 
 @bot.slash_command(name="камень", description="Выбрать камень в игре Камень, ножницы, бумага")
 async def rock(ctx):
     await play(ctx, "камень")
 
+
 @bot.slash_command(name="ножницы", description="Выбрать ножницы в игре Камень, ножницы, бумага")
 async def scissors(ctx):
     await play(ctx, "ножницы")
+
 
 @bot.slash_command(name="бумага", description="Выбрать бумагу в игре Камень, ножницы, бумага")
 async def paper(ctx):
     await play(ctx, "бумага")
 
-async def play(ctx, choice):
-    if not hasattr(bot, 'game_started') or not bot.game_started:
-        await ctx.send("Игра еще не началась! Для начала игры используйте команду `/игра`.")
-        return
 
-    choices = ['камень', 'ножницы', 'бумага']
+async def play(ctx, choice):
+    choices = ['камень', 'бумага', 'ножницы']
     bot_choice = random.choice(choices)
 
-    if choice.lower() not in choices:
-        await ctx.send("Неправильный выбор! Попробуйте снова: камень, ножницы или бумага.")
-        return
+    out = f"Вы выбрали: {choice}. Бот выбрал: {bot_choice}.\n"
 
-    await ctx.send(f"Вы выбрали: {choice}. Бот выбрал: {bot_choice}.")
-
-    if choice.lower() == bot_choice:
-        await ctx.send("Ничья!")
-    elif (choice.lower() == 'камень' and bot_choice == 'ножницы') or \
-        (choice.lower() == 'ножницы' and bot_choice == 'бумага') or \
-        (choice.lower() == 'бумага' and bot_choice == 'камень'):
-        await ctx.send("Вы победили!")
+    if choice == bot_choice:
+        out += "Ничья!"
+    elif (choices.index(bot_choice) + 1) % 3 == choices.index(choice):
+        out += "Вы победили!"
     else:
-        await ctx.send("Бот победил!")
+        out += "Бот победил!"
+
+    await ctx.send(out)
 
 
 token = ''
